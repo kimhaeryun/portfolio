0ebd0dcdd70ef7178aa1a75be7d144e0318d5a37
kimhaeryun <haeryun96@naver.com>
2021-07-16 19:47:19 +0900

    1
    1

--- two_site/folder/script.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-    $('.right').click(function () {
-        $('.fa fa-window-minimize').hide();
-    });
-    $('.right').click(function () {
-        $('.fa fa-window-minimize').show();
-    });
-});
